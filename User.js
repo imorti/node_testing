695b198aac608bb62345e363e2039558508d836f
ian.mortimer@yieldmo.com <ian.mortimer@yieldmo.com>
2017-06-20 11:40:26 -0400

    updated gitignore

---
+++ User.js
@@ -0,0 +1,16 @@
+/**
+ * Created by imorti on 6/20/17.
+ */
+'use strict'
+
+function up () {
+
+    return new Promise(function (resolve) {
+        resolve()
+    })
+}
+
+
+module.exports = {
+    up
+}
