a1173a0a8784a6dd956e102d9be6d7896cb449bf
ian.mortimer@yieldmo.com <ian.mortimer@yieldmo.com>
2017-06-20 11:51:59 -0400

    added knex.

---
+++ database.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const createKnex = require('knex')
+
+const knex = createKnex({
+    client: 'pg',
+    connection: 'postgres://@localhost:5432/nodejs_at_scale'
+})
+
+module.export knex
+
